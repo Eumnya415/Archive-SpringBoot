8b708d56b2ace784d458f0ad062457e255397c6f
Eumnya <2124524@naver.com>
2024-01-29 11:06:57 +0900

    Add(feature/notice): 공지사항 도메인 객체 추가

---
+++ src/main/java/com/sloop/archive/notice/domain/NoticeDTO.java
@@ -0,0 +1,25 @@
+package com.sloop.archive.notice.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@ToString
+public class NoticeDTO {
+    private Long id;                   // 공지사항 아이디
+    private Long userId;               // 공지사항 작성자 아이디
+    private String title;              // 공지사항 제목
+    private String content;            // 공지사항 내용 (text)
+    private int views;                 // 공지사항 조회수
+    private Timestamp registDate;      // 공지사항 등록 일시
+    private Boolean pinned;            // 공지사항 상단 고정 여부. 고정 1, 기본값 0
+    private Boolean updateFlag;        // 공지사항 수정 여부. 수정 1, 수정없음 0
+    private Timestamp updateDate;      // 공지사항 수정 일시
+    private Long updateUserId;         // 공지사항 수정자 아이디
+    private Boolean deleteFlag;        // 공지사항 삭제 여부. 삭제 1, 삭제없음 0
+    private Timestamp deleteDate;      // 공지사항 삭제 일시
+}
