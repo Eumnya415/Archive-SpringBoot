68e1d76599556105d2f07e5ac95a947578d0e251
CHA <2124524@gmail.com>
2024-01-31 10:55:45 +0900

    Docs(feature/notice): NoticeService 코드 수정

--- src/main/java/com/sloop/archive/notice/service/NoticeService.java
+++ src/main/java/com/sloop/archive/notice/service/NoticeService.java
@@ -1,13 +1,37 @@
 package com.sloop.archive.notice.service;
 
 import com.sloop.archive.notice.domain.NoticeDTO;
+import com.sloop.archive.notice.mapper.NoticeMapper;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface NoticeService {
-    List<NoticeDTO> getAllNoticePinnedFirst();
-    NoticeDTO getNoticeById(Long id);
-    void createNotice(NoticeDTO notice);
-    String updateNotice(Long id, NoticeDTO notice);
-    void deleteNotice(Long id);
+@Service
+public class NoticeService {
+
+    private final NoticeMapper noticeMapper;
+
+    public NoticeService(NoticeMapper noticeMapper) {
+        this.noticeMapper = noticeMapper;
+    }
+
+    public List<NoticeDTO> getAllNoticePinnedFirst() {
+        return noticeMapper.getAllNoticePinnedFirst();
+    }
+
+    public NoticeDTO getNoticeById(Long id) {
+        return noticeMapper.getNoticeById(id);
+    }
+
+    public void saveNotice(NoticeDTO notice) {
+        noticeMapper.saveNotice(notice);
+    }
+
+    public void updateNotice(NoticeDTO notice) {
+        noticeMapper.updateNotice(notice);
+    }
+
+    public void deleteNotice(Long id) {
+        noticeMapper.deleteNotice(id);
+    }
 }
