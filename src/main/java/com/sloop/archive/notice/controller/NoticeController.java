45ceb513bf5db4859d0296ad7b08b16607d7c084
CHA <2124524@gmail.com>
2024-01-31 13:49:20 +0900

    Docs(feature/notice): NoticeController 수정중

--- src/main/java/com/sloop/archive/notice/controller/NoticeController.java
+++ src/main/java/com/sloop/archive/notice/controller/NoticeController.java
@@ -36,10 +36,24 @@ public class NoticeController {
      * @return /notice/list
      */
     @GetMapping("/list")
-    public String getAllNoticePinnedFirst(Model model) {
-        model.addAttribute("noticeList", noticeService.getAllNoticePinnedFirst());
+    public String getAllNoticePinnedFirst(Model model, @RequestParam(defaultValue = "1") int page) {
+        int pageSize = 10; // 페이지 당 게시글 수
+        int totalCount = noticeService.getTotalCount(); // 전체 게시글 수
+
+        // 페이징 처리를 위한 계산
+        int start = (page - 1) * pageSize;
+        int totalPages = (int) Math.ceil((double) totalCount / pageSize);
+
+        List<NoticeDTO> noticeList = noticeService.getAllNoticePinnedFirst(start, pageSize);
+
+        model.addAttribute("noticeList", noticeList);
+        model.addAttribute("totalPages", totalPages);
+
         log.info("공지사항 목록 조회"); // 로그 기록 추가
         return "notice/list";
+
+//        model.addAttribute("noticeList", noticeService.getNoticePinnedFirst());
+
     }
 
 
@@ -53,24 +67,42 @@ public class NoticeController {
     @GetMapping("/view/{id}")
     public String getNoticeById(@PathVariable Long id, Model model) {
         model.addAttribute("notice", noticeService.getNoticeById(id));
+        log.info("공지사항 상세 조회"); // 로그 기록 추가
         return "notice/view";
+    }
+
+    // 공지사항 등록 페이지
+    @GetMapping("/saveForm")
+    public String showNoticeForm(Model model) {
+        model.addAttribute("notice", new NoticeDTO());
+        return "notice/save";
     }
 
     @PostMapping("/save")
     public String saveNotice(@ModelAttribute NoticeDTO noticeDTO) {
         noticeService.saveNotice(noticeDTO);
+        log.info("공지사항 작성"); // 로그 기록 추가
         return "redirect:/notice/list";
+    }
+
+    // 공지사항 수정 페이지
+    @GetMapping("/update/{id}")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        model.addAttribute("notice", noticeService.getNoticeById(id));
+        return "notice/update";
     }
 
     @PostMapping("/update")
     public String updateNotice(@ModelAttribute NoticeDTO noticeDTO) {
         noticeService.updateNotice(noticeDTO);
+        log.info("공지사항 수정"); // 로그 기록 추가
         return "redirect:/notice/view/" + noticeDTO.getId();
     }
 
     @PostMapping("/delete/{id}")
     public String deleteNotice(@PathVariable Long id) {
         noticeService.deleteNotice(id);
+        log.info("공지사항 삭제"); // 로그 기록 추가
         return "redirect:/notice/list";
     }
 
